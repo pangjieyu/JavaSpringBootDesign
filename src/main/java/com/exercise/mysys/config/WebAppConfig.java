519f6704be13011cc69a139d485efbfbcf7d7c9f
PangJieyu <PangJieyu@github.com>
2018-07-22 09:13:38 +0800

    fix charset problem

---
+++ src/main/java/com/exercise/mysys/config/WebAppConfig.java
@@ -0,0 +1,28 @@
+package com.exercise.mysys.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
+/**
+ * @author 庞界宇
+ * @ProjectName 食品企业订货销售系统
+ * @date 2018/7/22
+ */
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+        return converter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        converters.add(responseBodyConverter());
+    }
+}
