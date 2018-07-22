da2a2671e497d39751020480ed39999468d06fbc
zgg548 <35549898+zgg548@users.noreply.github.com>
2018-07-22 19:28:50 +0800

    find

--- src/main/java/com/exercise/mysys/service/findServices.java
+++ src/main/java/com/exercise/mysys/service/findServices.java
@@ -1,6 +1,5 @@
 package com.exercise.mysys.service;
-import com.exercise.mysys.domain.goodandstore;
-import com.exercise.mysys.domain.orderUserCustomGood;
+import com.exercise.mysys.domain.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -81,6 +80,179 @@ public class findServices {
     public static ArrayList<orderUserCustomGood> findOrder(String customName, String sysUserName) {
         ArrayList<orderUserCustomGood> list = new ArrayList<>();
 
+        Connection con = null ;
+
+        Statement stmt = null ;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");   //mysql为例 不一样的数据库所需的驱动包不一样 连接语句略有不同
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", "pangjieyu");
+            stmt = con.createStatement();
+            String sql2="select x.name, y.name, z.name, hh.deposit, hh.money, hh.number," +
+                    "hh.effective, hh.create_date, hh.payment from\n" +
+                    "order_good as hh, good as x, customer as y, sys_user as z\n" +
+                    "where hh.good_id=x.id and hh.customer_id = y.id and hh.employee_id = z.id ";
+            if (!customName.equals(""))
+            {
+                sql2 += "and y.name like '%" + customName + "%'";
+            }
+            if (!sysUserName.equals(""))
+            {
+                sql2 += "and z.name like '%" + sysUserName + "%'";
+            }
+            ResultSet rs = stmt.executeQuery(sql2);
+            while(rs.next())
+            {
+                orderUserCustomGood tem = new orderUserCustomGood();
+                tem.good_name = rs.getString(1);
+                tem.customer_name = rs.getString(2);
+                tem.employee_name = rs.getString(3);
+                tem.deposit = rs.getString(4);
+                tem.money = rs.getString(5);
+                tem.number = rs.getString(6);
+                tem.effective = rs.getString(7);
+                tem.create_date = rs.getString(8);
+                tem.payment = rs.getString(9);
+                list.add(tem);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.toString());
+            return null;
+        }
         return list;
     }
+
+    public static ArrayList<returnGoodResult> findReturn(String customName, String sysUserName) {
+        ArrayList<returnGoodResult> list = new ArrayList<>();
+
+        Connection con = null ;
+
+        Statement stmt = null ;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");   //mysql为例 不一样的数据库所需的驱动包不一样 连接语句略有不同
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", "pangjieyu");
+            stmt = con.createStatement();
+            String sql2="select x.name, y.name, z.name, hh.money, hh.number," +
+                    "hh.effective, hh.createdate from\n" +
+                    "return_good as hh, good as x, customer as y, sys_user as z\n" +
+                    "where hh.good_id=x.id and hh.customer_id = y.id and hh.employee_id = z.id ";
+            if (!customName.equals(""))
+            {
+                sql2 += "and y.name like '%" + customName + "%'";
+            }
+            if (!sysUserName.equals(""))
+            {
+                sql2 += "and z.name like '%" + sysUserName + "%'";
+            }
+            ResultSet rs = stmt.executeQuery(sql2);
+            while(rs.next())
+            {
+                returnGoodResult tem = new returnGoodResult();
+                tem.good_name = rs.getString(1);
+                tem.customer_name = rs.getString(2);
+                tem.employee_name = rs.getString(3);
+                tem.money = rs.getString(4);
+                tem.number = rs.getString(5);
+                tem.effective = rs.getString(6);
+                tem.create_date = rs.getString(7);
+                list.add(tem);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.toString());
+            return null;
+        }
+        return list;
+    }
+
+    public static ArrayList<pickGoodResult> findPick(String customName, String sysUserName) {
+        ArrayList<pickGoodResult> list = new ArrayList<>();
+
+        Connection con = null ;
+
+        Statement stmt = null ;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");   //mysql为例 不一样的数据库所需的驱动包不一样 连接语句略有不同
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", "pangjieyu");
+            stmt = con.createStatement();
+            String sql2="select x.name, y.name, z.name, hh.section, hh.number," +
+                    "hh.effective, hh.create_date from\n" +
+                    "pick as hh, good as x, customer as y, sys_user as z\n" +
+                    "where hh.good_id=x.id and hh.customer_id = y.id and hh.employee_id = z.id ";
+            if (!customName.equals(""))
+            {
+                sql2 += "and y.name like '%" + customName + "%'";
+            }
+            if (!sysUserName.equals(""))
+            {
+                sql2 += "and z.name like '%" + sysUserName + "%'";
+            }
+            ResultSet rs = stmt.executeQuery(sql2);
+            while(rs.next())
+            {
+                pickGoodResult tem = new pickGoodResult();
+                tem.good_name = rs.getString(1);
+                tem.customer_name = rs.getString(2);
+                tem.employee_name = rs.getString(3);
+                tem.section = rs.getString(4);
+                tem.number = rs.getString(5);
+                tem.effective = rs.getString(6);
+                tem.create_date = rs.getString(7);
+                list.add(tem);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.toString());
+            return null;
+        }
+        return list;
+    }
+
+//    public static ArrayList<manufacturePlanResult> findManufacturePlan(String customName, String sysUserName) {
+//        ArrayList<pickGoodResult> list = new ArrayList<>();
+//
+//        Connection con = null ;
+//
+//        Statement stmt = null ;
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");   //mysql为例 不一样的数据库所需的驱动包不一样 连接语句略有不同
+//            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", "pangjieyu");
+//            stmt = con.createStatement();
+//            String sql2="select x.name, y.name, z.name, hh.section, hh.number," +
+//                    "hh.effective, hh.create_date from\n" +
+//                    "pick as hh, good as x, customer as y, sys_user as z\n" +
+//                    "where hh.good_id=x.id and hh.customer_id = y.id and hh.employee_id = z.id ";
+//            if (!customName.equals(""))
+//            {
+//                sql2 += "and y.name like '%" + customName + "%'";
+//            }
+//            if (!sysUserName.equals(""))
+//            {
+//                sql2 += "and z.name like '%" + sysUserName + "%'";
+//            }
+//            ResultSet rs = stmt.executeQuery(sql2);
+//            while(rs.next())
+//            {
+//                pickGoodResult tem = new pickGoodResult();
+//                tem.good_name = rs.getString(1);
+//                tem.customer_name = rs.getString(2);
+//                tem.employee_name = rs.getString(3);
+//                tem.section = rs.getString(4);
+//                tem.number = rs.getString(5);
+//                tem.effective = rs.getString(6);
+//                tem.create_date = rs.getString(7);
+//                list.add(tem);
+//            }
+//        }
+//        catch (Exception e)
+//        {
+//            System.out.println(e.toString());
+//            return null;
+//        }
+//        return list;
+//    }
+
 }
