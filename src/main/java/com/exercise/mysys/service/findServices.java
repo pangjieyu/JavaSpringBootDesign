7105de704a37e7ade96622b1ed285c2e2ac9aaab
PangJieyu <PangJieyu@github.com>
2018-07-23 18:03:21 +0800

    整合密码

--- src/main/java/com/exercise/mysys/service/findServices.java
+++ src/main/java/com/exercise/mysys/service/findServices.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class findServices {
+    private static String password = "pangjieyu";
     public static ArrayList<goodandstore> findGood(String name) {
 
         ArrayList<goodandstore> list = new ArrayList<>();
@@ -18,7 +19,7 @@ public class findServices {
         try {
             System.out.println("enter");
             Class.forName("com.mysql.jdbc.Driver");   //mysql为例 不一样的数据库所需的驱动包不一样 连接语句略有不同
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", "123456789");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", password);
             stmt = con.createStatement();
             String sql2="select x.good_id,x.in_id,x.in_time,x.number,x.position,y.name,x.id from\n" +
                     "store as x,good as y\n" +
@@ -53,7 +54,7 @@ public class findServices {
         try {
             System.out.println("enter");
             Class.forName("com.mysql.jdbc.Driver");   //mysql为例 不一样的数据库所需的驱动包不一样 连接语句略有不同
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", "123456789");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", password);
             stmt = con.createStatement();
             String sql2="select x.id,x.in_id,x.in_time,x.number,x.position,y.name from\n" +
                     "store as x,good as y\n" +
@@ -87,7 +88,7 @@ public class findServices {
         Statement stmt = null ;
         try {
             Class.forName("com.mysql.jdbc.Driver");   //mysql为例 不一样的数据库所需的驱动包不一样 连接语句略有不同
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", "123456789");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", password);
             stmt = con.createStatement();
             String sql2="select x.name, y.name, z.name, hh.deposit, hh.money, hh.number," +
                     "hh.effective, hh.create_date, hh.payment from\n" +
@@ -133,7 +134,7 @@ public class findServices {
         Statement stmt = null ;
         try {
             Class.forName("com.mysql.jdbc.Driver");   //mysql为例 不一样的数据库所需的驱动包不一样 连接语句略有不同
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", "123456789");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", password);
             stmt = con.createStatement();
             String sql2="select x.name, y.name, z.name, hh.money, hh.number," +
                     "hh.effective, hh.createdate from\n" +
@@ -177,7 +178,7 @@ public class findServices {
         Statement stmt = null ;
         try {
             Class.forName("com.mysql.jdbc.Driver");   //mysql为例 不一样的数据库所需的驱动包不一样 连接语句略有不同
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", "123456789");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", password);
             stmt = con.createStatement();
             String sql2="select x.name, y.name, z.name, hh.section, hh.number," +
                     "hh.effective, hh.create_date from\n" +
@@ -221,7 +222,7 @@ public class findServices {
         Statement stmt = null ;
         try {
             Class.forName("com.mysql.jdbc.Driver");   //mysql为例 不一样的数据库所需的驱动包不一样 连接语句略有不同
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", "123456789");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", password);
             stmt = con.createStatement();
             String sql2="select x.name, y.name, hh.number, hh.money," +
                     "hh.effective, hh.manufacture_date from\n" +
@@ -264,7 +265,7 @@ public class findServices {
         Statement stmt = null ;
         try {
             Class.forName("com.mysql.jdbc.Driver");   //mysql为例 不一样的数据库所需的驱动包不一样 连接语句略有不同
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", "123456789");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", password);
             stmt = con.createStatement();
             String sql2="select x.name, hh.money," +
                     "hh.effective, hh.receivable, hh.type, hh.id from\n" +
@@ -307,7 +308,7 @@ public class findServices {
         Statement stmt = null ;
         try {
             Class.forName("com.mysql.jdbc.Driver");   //mysql为例 不一样的数据库所需的驱动包不一样 连接语句略有不同
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", "123456789");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", password);
             stmt = con.createStatement();
             String sql2="select x.name, y.name, hh.in_date," +
                     "hh.number from\n" +
@@ -352,7 +353,7 @@ public class findServices {
         Statement stmt = null ;
         try {
             Class.forName("com.mysql.jdbc.Driver");   //mysql为例 不一样的数据库所需的驱动包不一样 连接语句略有不同
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", "123456789");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sqlDesign?useUnicode=true&characterEncoding=utf-8", "root", password);
             stmt = con.createStatement();
             String sql2="select x.name, y.name, hh.out_date," +
                     "hh.number from\n" +
